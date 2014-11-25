6fcb80bb67684f70dbf9050b6ddd6b7d0d4efbbd
Antaril <Antaril@gmx.de>
2014-11-25 22:49:10 +0100

    add lost scheduler

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 13 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#13 SMP PREEMPT Tue Nov 25 22:12:48 CET 2014"
+#define UTS_VERSION "#14 SMP PREEMPT Tue Nov 25 22:29:13 CET 2014"
 #define LINUX_COMPILE_BY "antaril"
 #define LINUX_COMPILE_HOST "Antaril-AGK-Edition"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
