87996caa03f23c708f5837446b9acec21fc6c3a7
Antaril <Antaril@gmx.de>
2014-11-30 10:31:28 +0100

    complete adding kexec hardboot

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 45 */
+/* This file is auto generated, version 46 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#45 SMP PREEMPT Sat Nov 29 20:48:42 CET 2014"
+#define UTS_VERSION "#46 SMP PREEMPT Sun Nov 30 09:37:43 CET 2014"
 #define LINUX_COMPILE_BY "antaril"
 #define LINUX_COMPILE_HOST "Antaril-AGK-Edition"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
