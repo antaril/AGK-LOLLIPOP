87a1017a98faf0ab2b43909b8ce9144025bb74e0
Antaril <Antaril@gmx.de>
2014-12-04 22:57:59 +0100

    add cpufreq_intellimm: intelliminmax cpu governor
    intelliminmax (intellimm) governor is designed to work with the newer SOCs with fixed voltage rails (ie MSM8974+ SOCs).  It is designed to work within those
    fixed voltage ranges in order to maximize battery performance while creating a
    smooth UI operations.

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 46 */
+/* This file is auto generated, version 50 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#46 SMP PREEMPT Sun Nov 30 09:37:43 CET 2014"
+#define UTS_VERSION "#50 SMP PREEMPT Thu Dec 4 22:04:39 CET 2014"
 #define LINUX_COMPILE_BY "antaril"
 #define LINUX_COMPILE_HOST "Antaril-AGK-Edition"
 #define LINUX_COMPILER "gcc version 4.7 (GCC) "
